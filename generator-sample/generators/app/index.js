8bebdbbc94d889164b011f14e4f0cd57642ab0d0
aicherish0320 <aicherish0320@163.com>
2022-02-15 06:36:47 +0800

    feat: yo 接口用户输入

--- generator-sample/generators/app/index.js
+++ generator-sample/generators/app/index.js
@@ -8,19 +8,41 @@
 const Generator = require('yeoman-generator')
 
 module.exports = class extends Generator {
+  prompting() {
+    return this.prompt([
+      {
+        type: 'input',
+        name: 'name',
+        message: 'Your project name',
+        default: this.appname // appname 为项目生产目录名称
+      }
+    ]).then((answers) => {
+      this.answers = answers
+    })
+  }
   writing() {
     /*
       Yeoman 自动在生成文件阶段调用此方法
       我们这里尝试往项目中写入文件
     */
     // this.fs.write(this.destinationPath('temp.txt'), Math.random().toString())
+    // // 通过模版方式写入文件到目标目录
+    // // 模板文件路径
+    // const tmpl = this.templatePath('foo.txt')
+    // // 输出目标路径
+    // const output = this.destinationPath('foo.txt')
+    // // 模板数据上下文
+    // const context = { title: 'Hello aic', success: false }
+
+    // this.fs.copyTpl(tmpl, output, context)
+
     // 通过模版方式写入文件到目标目录
     // 模板文件路径
-    const tmpl = this.templatePath('foo.txt')
+    const tmpl = this.templatePath('bar.html')
     // 输出目标路径
-    const output = this.destinationPath('foo.txt')
+    const output = this.destinationPath('bar.html')
     // 模板数据上下文
-    const context = { title: 'Hello aic', success: false }
+    const context = this.answers
 
     this.fs.copyTpl(tmpl, output, context)
   }
